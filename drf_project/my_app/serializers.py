0c3e7bea29ec0dc0dcd3425c7b2996d41966434c
Nipesh <nipeshincbca2021@gmai.com>
2026-01-11 12:16:44 +0545

    get and post operation commit

--- drf_project/my_app/serializers.py
+++ drf_project/my_app/serializers.py
@@ -1,2 +1,9 @@
+from rest_framework import serializers
+from .models import Product
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['title','category','price']
 
 
---
+++ drf_project/my_app/views/main_view.py
@@ -0,0 +1,30 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from ..serializers import ProductSerializer
+from rest_framework import status
+from ..models import Product
+
+@api_view(['GET','POST'])
+def product_view(request):
+    if request.method == 'GET':
+        product = Product.objects.all()
+        print(product)
+        serializer = ProductSerializer(product,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    
+    # for post request
+    if request.method == 'POST':
+        # title = request.data.get('title')
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':"Data posted successfully",'data':serializer.data},status=status.HTTP_201_CREATED)
+        else:
+            return Response({'error':serializer.errors},status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+
+
+
+
